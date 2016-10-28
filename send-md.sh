d0bcfba8d725f77c28cccd1dea75b2c22c8fadf2
mth91 <marvin.hoffmann@stud.uni-heidelberg.de>
2016-10-28 11:32:24 +0200

    Update send-md.sh

--- send-md.sh
+++ send-md.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts "s:f:i:" OPTION
+while getopts "s:f:i:n:" OPTION
 do
 	case $OPTION in
 		s)
@@ -15,12 +15,16 @@ do
 			echo The value of -i is $OPTARG
 			timeinterval=$OPTARG
 			;;
+		n)
+			echo The project name is $OPTARG
+			projectname=$OPTARG
+			;;
 	esac
 done
 
 until [ $startpoint -gt $endpoint ]; do
-	grompp -f md-vibration.mdp -c npt-tr2-u-${startpoint}.gro -t npt-tr2-u-${startpoint}.cpt -p topol.top -n index.ndx -o md-tr2-u-${startpoint}.tpr
-	send_MD_job -q "-l nodes=1:amd:ppn=8,vmem=4gb,mem=4gb,walltime=12:00:00" md-tr2-u-${startpoint}
+	grompp -f md-vibration.mdp -c npt-${projectname}-${startpoint}.gro -t npt-${projectname}-${startpoint}.cpt -p topol.top -n index.ndx -o md-${projectname}-${startpoint}.tpr
+	send_MD_job -q "-l nodes=1:amd:ppn=8,vmem=4gb,mem=4gb,walltime=12:00:00" md-${projectname}-${startpoint}
 	let startpoint=$startpoint+$timeinterval
 	#startpoint=$(($startpoint + $timeinterval))
 done

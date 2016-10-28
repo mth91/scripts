06f528124348751d767e37dc5d3897ff33039f74
mth91 <marvin.hoffmann@stud.uni-heidelberg.de>
2016-10-28 12:22:30 +0200

    added comments

--- distances.sh
+++ distances.sh
@@ -18,9 +18,14 @@ do
 	esac
 done
 until [ $startsnap -gt $endsnap ]; do
+	# execute g_dist, print out distance between given index groups 
+	# (input via groups.txt so no manual interaction for every single run is needed)
 	g_dist -s pull-25.tpr -f conf${startsnap}.gro -n index.ndx -o dist${startsnap}.xvg < groups.txt &>/dev/null
+	# parse generated .xvg file into awk and print the timestep and distance into summary-distances.dat
 	awk '/^[^@#]/ {print $1 " " $2}' dist${startsnap}.xvg >> summary-distances.dat
+	# delete previously generated .xvg file
 	rm dist${startsnap}.xvg
+	# increment counter
 	let startsnap=$startsnap+$timeinterval
 done
 echo Hooray! Finished!
